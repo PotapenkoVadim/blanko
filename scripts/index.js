7afa090c584eadd725958e9df7ce7d82698c901e
Vadim Potapenko <vpotapenko@ronasit.com>
2022-11-22 14:36:02 +0400

    fix: fix code style

--- scripts/index.js
+++ scripts/index.js
@@ -3,26 +3,26 @@ const burgerMenu = document.querySelector('#burgerMenu');
 const closeMenu = document.querySelector('#closeMenu');
 const headerList = document.querySelector('#headerList');
 
-burgerMenu.addEventListener('click', toggleMobileMenu);
-closeMenu.addEventListener('click', closeMobileMenu);
-
-function toggleMobileMenu() {
-  return headerList.classList.toggle('header__list-visible');
-}
-
-function closeMobileMenu() {
-  return headerList.classList.remove('header__list-visible');
-}
+burgerMenu.addEventListener('click', () => headerList.classList.toggle('header__list-visible'));
+closeMenu.addEventListener('click', () => headerList.classList.remove('header__list-visible'));
 
 
 // gallery
 let galleryIndex = 0;
+const GALLERY_TIME_DELAY = 12500;
 const gallery = document.querySelector('#gallery');
 const bullets = document.querySelectorAll('#bullet');
 
 [...bullets].map((bullet) => bullet.addEventListener('click', handleBulletClick));
-setInterval(() => slideGallery(galleryIndex = 1 - galleryIndex ), 12500);
-addEventListener('resize', debounce(() => slideGallery(galleryIndex = 1 - galleryIndex)));
+
+setInterval(() => slideGallery(getGalleryIndex()), GALLERY_TIME_DELAY);
+addEventListener('resize', debounce(() => slideGallery(getGalleryIndex())));
+
+function getGalleryIndex() {
+  galleryIndex = 1 - galleryIndex;
+
+  return galleryIndex;
+}
 
 function handleBulletClick() {
   galleryIndex = this.dataset.index;
@@ -35,7 +35,7 @@ function slideGallery(index) {
   gallery.style.transform = `translateX(${galleryItemWidth * index * -1}px)`;
 }
 
-function debounce(fn, timeout = 150) {
+function debounce(fn, timeout = 250) {
   let timer;
 
   return (...args) => {
