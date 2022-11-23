acd107b0182aa07e430147edc53272d14b112872
Vadim Potapenko <vpotapenko@ronasit.com>
2022-11-23 15:00:50 +0400

    refact: scripts refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,89 @@
-// mobile menu
-const burgerMenu = document.querySelector('#burgerMenu');
-const closeMenu = document.querySelector('#closeMenu');
-const headerList = document.querySelector('#headerList');
-
-burgerMenu.addEventListener('click', () => headerList.classList.toggle('header__list-visible'));
-closeMenu.addEventListener('click', () => headerList.classList.remove('header__list-visible'));
-
-
-// gallery
-let galleryIndex = 0;
-const GALLERY_TIME_DELAY = 12500;
-const gallery = document.querySelector('#gallery');
-const bullets = document.querySelectorAll('#bullet');
-
-[...bullets].map((bullet) => bullet.addEventListener('click', handleBulletClick));
-
-setInterval(() => slideGallery(getGalleryIndex()), GALLERY_TIME_DELAY);
-addEventListener('resize', debounce(() => slideGallery(getGalleryIndex())));
-
-function getGalleryIndex() {
-  galleryIndex = 1 - galleryIndex;
-
-  return galleryIndex;
+class DOMService {
+  static getElementByID(id) {
+    return id ? document.getElementById(id) : null;
+  }
 }
 
-function handleBulletClick() {
-  galleryIndex = this.dataset.index;
+class HTMLElement {
+  _element = null;
 
-  slideGallery(galleryIndex);
+  constructor(id) {
+    this._element = DOMService.getElementByID(id);
+  }
+
+  get element() {
+    return this._element;
+  }
+
+  handleClick(callback) {
+    try {
+      return this._element && this._element.addEventListener('click', callback);
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+
+  toggleClassStyle(classStyle) {
+    return this._element && this._element.classList.toggle(classStyle);
+  }
+
+  removeClassStyle(classStyle) {
+    return this._element && this._element.classList.remove(classStyle);
+  }
+
+  setStyle(key, value) {
+    return this._element.style[key] = value;
+  }
 }
 
-function slideGallery(index) {
-  const galleryItemWidth = document.querySelector('#item').clientWidth;
-  gallery.style.transform = `translateX(${galleryItemWidth * index * -1}px)`;
+class Gallery extends HTMLElement {
+  _index = 0;
+  slider = null;
+
+  constructor(id) {
+    super(id);
+
+    this.slider = new HTMLElement('slider');
+  }
+
+  get sliderWidth() {
+    return this.slider.element.clientWidth;
+  }
+
+  calculateIndex(number) {
+    this._index = number ? number : 1 - this._index;
+
+    return this._index;
+  }
+
+  slide(number) {
+    const index = this.calculateIndex(number);
+
+    this.setStyle('transform', `translateX(${this.sliderWidth * index * -1}px)`);
+  }
 }
 
+class Bullet extends HTMLElement {
+  gallery = null;
+
+  constructor(id, galleryElement) {
+    super(id);
+    
+    this.gallery = galleryElement;
+  }
+
+  get bulletIndex() {
+    return this.element.dataset.index;
+  }
+
+  handleBulletClick() {
+    return this.handleClick(() => {
+      this.gallery.slide(this.bulletIndex);
+    });
+  }
+}
+
+// helpers
 function debounce(fn, timeout = 250) {
   let timer;
 
@@ -44,3 +93,23 @@ function debounce(fn, timeout = 250) {
     timer = setTimeout(() => fn.apply(this, args), timeout);
   };
 }
+
+/* ### Burger menu ### */
+const GALLERY_TIME_DELAY = 12500;
+const burgerMenu = new HTMLElement('burgerMenu');
+const closeMenu = new HTMLElement('closeMenu');
+const headerList = new HTMLElement('headerList');
+
+burgerMenu.handleClick(() => headerList.toggleClassStyle('header__list-visible'));
+closeMenu.handleClick(() => headerList.removeClassStyle('header__list-visible'));
+
+/* ### Gallery ### */
+const gallery = new Gallery('gallery');
+const rightBullet = new Bullet('rightBullet', gallery);
+const leftBullet = new Bullet('leftBullet', gallery);
+
+rightBullet.handleBulletClick();
+leftBullet.handleBulletClick();
+
+setInterval(() => gallery.slide(), GALLERY_TIME_DELAY);
+addEventListener('resize', debounce(() => gallery.slide()));
